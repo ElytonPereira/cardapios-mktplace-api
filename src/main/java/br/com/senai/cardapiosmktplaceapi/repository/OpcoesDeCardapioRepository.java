c1cd12fecb7dda83c8bbd96131335bf58353eb47
Elyton <fernandeselyton@gmail.com>
2023-09-13 22:13:02 -0300

    Finalizado a camada service das opções de cardapios e o cadastros de cardapios

--- src/main/java/br/com/senai/cardapiosmktplaceapi/repository/OpcoesDeCardapioRepository.java
+++ src/main/java/br/com/senai/cardapiosmktplaceapi/repository/OpcoesDeCardapioRepository.java
@@ -1,14 +1,18 @@
 package br.com.senai.cardapiosmktplaceapi.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import br.com.senai.cardapiosmktplaceapi.entity.Opcao;
+import br.com.senai.cardapiosmktplaceapi.entity.OpcaoDoCardapio;
+
 @Repository
-public interface OpcoesDeCardapioRepository {
+public interface OpcoesDeCardapioRepository extends JpaRepository<OpcaoDoCardapio, Integer>{
 
 	@Query(value = "SELECT Count(oc) "
 			+ "FROM OpcaoDoCardapio oc "
-			+ "WHERE oc.secao.id = :idDaOpcoesDeCardapio")
-	public Long contarPor(Integer idDaOpcoesDeCardapio);
+			+ "WHERE oc.secao.id = :idDaSecaoDoCardapio")
+	public Long contarPor(Integer idDaSecaoDoCardapio);
 	
 }
